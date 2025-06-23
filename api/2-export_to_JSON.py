588341c2d0cb3cdbd19ae118836bab82ccf56b9f
UShamila <s.uwineza@alustudent.com>
2025-06-23 04:10:21 -0700

    API

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 """
 Module to fetch user information and export TODO list to a JSON file
@@ -12,7 +11,7 @@ def get_employee_info(employee_id):
     """
     Get employee information by employee ID
     """
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
     response = requests.get(url)
     return response.json()
 
@@ -21,7 +20,7 @@ def get_employee_todos(employee_id):
     """
     Get the TODO list of the employee by employee ID
     """
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
     response = requests.get(url)
     return response.json()
 
@@ -30,7 +29,7 @@ def export_to_json(employee_id, todos):
     """
     Export TODO list to a JSON file
     """
-    filename = f"{employee_id}.json"
+    filename = "{}.json".format(employee_id)
     with open(filename, "w") as file:
         json.dump({employee_id: todos}, file)
 
@@ -41,51 +40,22 @@ def main(employee_id):
     """
     user_info = get_employee_info(employee_id)
     todos_info = get_employee_todos(employee_id)
-
-    employee_username = user_info["username"]
+    employee_username = user_info.get("username")
 
     todos_info_sorted = [
         {
-            "task": task["title"],
-            "completed": task["completed"],
+            "task": task.get("title"),
+            "completed": task.get("completed"),
             "username": employee_username
-        } for task in todos_info
+        }
+        for task in todos_info
     ]
 
     export_to_json(employee_id, todos_info_sorted)
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         main(sys.argv[1])
     else:
         print("Usage: ./2-export_to_JSON.py <employee_id>")
-=======
-#!/usr/bin/python3
-"""
-Uses https://jsonplaceholder.typicode.com along with an employee ID to
-return information about the employee's todo list progress
-"""
-
-import json
-import requests
-from sys import argv
-
-if __name__ == '__main__':
-    userId = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(userId), verify=False).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(userId), verify=False).json()
-    username = user.get('username')
-    tasks = []
-    for task in todo:
-        task_dict = {}
-        task_dict["task"] = task.get('title')
-        task_dict["completed"] = task.get('completed')
-        task_dict["username"] = username
-        tasks.append(task_dict)
-    jsonobj = {}
-    jsonobj[userId] = tasks
-    with open("{}.json".format(userId), 'w') as jsonfile:
-        json.dump(jsonobj, jsonfile)
->>>>>>> 2e0e743a0dd2aefc721d02f9ae7d2583cdc33a6b
